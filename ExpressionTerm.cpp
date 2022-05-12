b9b958d086b95f61990cb38e5f939a0b96bcfe6f
tWhite7217 <tommywhite525@gmail.com>
2022-05-11 21:49:59 -0400

    Renamed th termInfo class to ExpressionTerm and moved it to its own files so both flex files can use it. Significantly cleaned the postfix_to_llvm.l file in preparation for outputting llvm.

---
+++ ExpressionTerm.cpp
@@ -0,0 +1,11 @@
+#include "ExpressionTerm.h"
+
+bool ExpressionTerm::is_conditional_operator()
+{
+    return (type == TermType::OP) && (value == '?');
+}
+
+bool ExpressionTerm::is_not_left_parenthesis()
+{
+    return (type != TermType::OP) || (value != '(');
+}
---
+++ ExpressionTerm.h
@@ -0,0 +1,22 @@
+#include <string>
+
+enum class TermType
+{
+    OP,
+    NUM,
+    VAR
+};
+
+class ExpressionTerm
+{
+public:
+    TermType type;
+    int value;
+    std::string text;
+
+    ExpressionTerm(TermType type, int value, std::string text) : type(type), value(value), text(text) {}
+
+    bool is_conditional_operator();
+
+    bool is_not_left_parenthesis();
+};
