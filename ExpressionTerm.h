64b4299099607fb7d5328b43824fcb5ee2ba0fc2
tWhite7217 <tommywhite525@gmail.com>
2022-05-13 16:55:29 -0400

    Removed .0s from tmp variables. Created a new vector that tracks variables that are used before being defined.

--- ExpressionTerm.h
+++ ExpressionTerm.h
@@ -13,9 +13,13 @@ public:
     TermType type;
     int value;
     std::string text;
+    bool is_tmp;
 
     ExpressionTerm() = default;
-    ExpressionTerm(TermType type, int value, std::string text) : type(type), value(value), text(text) {}
+    ExpressionTerm(TermType type, int value, std::string text)
+        : type(type), value(value), text(text), is_tmp(false) {}
+    ExpressionTerm(TermType type, int value, std::string text, bool is_tmp)
+        : type(type), value(value), text(text), is_tmp(is_tmp) {}
 
     bool is_conditional_operator();
 
